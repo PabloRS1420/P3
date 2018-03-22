176e285dd470f3479c9f45f63c3678dcb0fc79b9
PabloRS1420 <36885631+PabloRS1420@users.noreply.github.com>
2018-03-22 13:54:51 +0100

    Create queue.h

---
+++ queue.h
@@ -0,0 +1,15 @@
+#indef QUEUE_H
+#define QUEUE_H
+
+typedef struct _Queue Queue;
+
+Queue *queue_ini(destroy_elementqueue_function_type f1, copy_elementqueue_function_type f2, print_elementqueue_function_type f3);
+void queue_destroy(Queue *q);
+Bool queue_isEmpty(const Queue *q);
+Bool queue_isFull(const Queue *queue);
+Status queue_insert(Queue *q, const void *pElem);
+void *queue_extract(Queue *q);
+int queue_size(const Queue *q);
+int queue_print(FILE *pf, const Queue *q);
+
+#endif QUEUE_H
