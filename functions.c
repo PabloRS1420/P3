ef7027d8e1404b22b79ac3b068469acf31257bf1
PabloRS1420 <36885631+PabloRS1420@users.noreply.github.com>
2018-04-18 19:06:52 +0200

    Create functions.c

---
+++ functions.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void destroy_element_function(void* e){ 
+  free((int*)e); 
+} 
+ 
+void *copy_element_function(const void* e){ 
+  int * dst; 
+  if (e == NULL) return NULL; 
+  dst = (int*)malloc(sizeof(int)); 
+  /*Copiamos el elemento*/ 
+  *(dst) = *((int*)e); 
+  return dst; 
+} 
+ 
+int print_element_function(FILE * f, const void* e){ 
+  if (f != NULL && e != NULL) {
+    return fprintf(f, "[%d]"\n, *((int*)e)); 
+  }
+  return -1; 
+} 
