8fac9f956fb84e72f631b027e0e7b7e9694243df
PabloRS1420 <36885631+PabloRS1420@users.noreply.github.com>
2018-04-19 00:05:31 +0200

    Update p3_e1.c

--- p3_e1.c
+++ p3_e1.c
@@ -17,9 +17,8 @@ int main(int argc, char * argv[]){
     Queue *q2 = queue_ini(destroy_element_function, copy_element_function, print_element_function);
     Queue *q3 = queue_ini(destroy_element_function, copy_element_function, print_element_function);
     Point *p = point_ini(0, 0, +);
-    int i, j, x, y;
+    int i, z, x, y;
     char s, l[3];
-    int z;
     char *token;
     if(queue_isEmpty(q1)) printf("Cola 1: Queue vacia.\n");
     else {
@@ -65,12 +64,49 @@ int main(int argc, char * argv[]){
         }
     }
     
-  
+    printf("<<<Pasando la primera mitad de Cola 1 a Cola 2");
+    z = z/2;
+    for(i=0; i<z; i++){
+        queue_insert(q2, queue_extract(q1));
+        if(queue_isEmpty(q1)) printf("Cola 1: Queue vacia.\n");
+        else {
+            printf("Cola 1: Cola con %d elementos:\n", queue_size(q1));
+            queue_print(fp, q1);
+        }
+        if(queue_isEmpty(q2)) printf("Cola 2: Queue vacia.\n");
+        else {
+            printf("Cola 2: Cola con %d elementos:\n", queue_size(q2));
+            queue_print(fp, q2);
+        }
+        if(queue_isEmpty(q3)) printf("Cola 3: Queue vacia.\n");
+        else {
+            printf("Cola 3: Cola con %d elementos:\n", queue_size(q3));
+            queue_print(fp, q3);
+        }
+    }
+    
+    printf("<<<Pasando la segunda mitad de Cola 1 a Cola 3");
+    while(queue_isEmpty(q1) != TRUE){
+        queue_insert(q3, queue_extract(q1));
+        if(queue_isEmpty(q1)) printf("Cola 1: Queue vacia.\n");
+        else {
+            printf("Cola 1: Cola con %d elementos:\n", queue_size(q1));
+            queue_print(fp, q1);
+        }
+        if(queue_isEmpty(q2)) printf("Cola 2: Queue vacia.\n");
+        else {
+            printf("Cola 2: Cola con %d elementos:\n", queue_size(q2));
+            queue_print(fp, q2);
+        }
+        if(queue_isEmpty(q3)) printf("Cola 3: Queue vacia.\n");
+        else {
+            printf("Cola 3: Cola con %d elementos:\n", queue_size(q3));
+            queue_print(fp, q3);
+        }
+    }
+    fclose(fp);
+    
+    return 0;
 }
 
 
-fgets
-{
-strtok
-sscanf
-}
