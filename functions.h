25bae2fd62823dfb87491b9fb5d186b5c6f3968b
PabloRS1420 <36885631+PabloRS1420@users.noreply.github.com>
2018-04-18 19:10:30 +0200

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,12 @@
+#indef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void destroy_element_function(void *e);
+void *copy_element_function(const void *e);
+int print_element_function(FILE *f, const void *e);
+
+#endif FUNCTIONS_H
